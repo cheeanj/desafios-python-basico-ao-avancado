489fed7449b86606d61e29bbff7e22f79f96bcb1
Carlos Jean <92583563+cheeanj@users.noreply.github.com>
2022-01-18 11:18:29 -0300

    Update desafio 2.py

--- desafio 2.py
+++ desafio 2.py
@@ -3,7 +3,7 @@
 # inteiro, informe que não é um número inteiro.
 """
 
-# num_int = input("Digite um numero: ")
+num_int = input("Digite um numero: ")
 
 
 # try:
@@ -15,16 +15,16 @@
 # except:
 #     print("O valor digitado não é um número inteiro")
 
-# if num_int.isdigit():
-#     num_int = int(num_int)
-#
-#     if num_int % 2 == 0:
-#         print(f"{num_int} é par")
-#     else:
-#         print(f"{num_int} é ímpar")
-#
-# else:
-#     print('Isso não é um número inteiro.')
+if num_int.isdigit():
+    num_int = int(num_int)
+
+    if num_int % 2 == 0:
+        print(f"{num_int} é par")
+    else:
+        print(f"{num_int} é ímpar")
+
+else:
+    print('Isso não é um número inteiro.')
 
 """
 # Faça um programa que pergunte a hora ao usuário e, baseando-se no horário
@@ -32,45 +32,45 @@
 # Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
 
-# hora = input('Que horas são? ')
+hora = input('Que horas são? ')
 
 
-# # hora = int(hora)
-#
-# # if hora >= 0 and hora <= 11:
-# #     print('Bom dia')
-# # elif hora >= 12 and hora <= 17:
-# #     print('Boa tarde')
-# # elif hora >= 18 and hora <= 23:
-# #     print('Boa noite')
-# # else:
-# #     print('Não existe essa hora')
-#
-# # if hora.isdigit():
-# #     hora = int(hora)
-# #
-# #     if hora < 0 or hora > 23:
-# #         print("horário deve estar entre 0 e 23")
-# #     else:
-# #         if hora <= 11:
-# #             print('Bom dia!')
-# #         elif hora <= 17:
-# #             print('Boa tarde!')
-# #         else:
-# #             print('boa noite!')
-#
-# try:
+# hora = int(hora)
+
+# if hora >= 0 and hora <= 11:
+#     print('Bom dia')
+# elif hora >= 12 and hora <= 17:
+#     print('Boa tarde')
+# elif hora >= 18 and hora <= 23:
+#     print('Boa noite')
+# else:
+#     print('Não existe essa hora')
+
+# if hora.isdigit():
 #     hora = int(hora)
-#     if 0 <= hora <= 11:
-#         print('Bom dia')
-#     elif 12 <= hora <= 17:
-#         print('Boa tarde')
-#     elif 18 <= hora <= 23:
-#         print('Boa noite')
+#
+#     if hora < 0 or hora > 23:
+#         print("horário deve estar entre 0 e 23")
 #     else:
-#         print('horário invalido')
-# except:
-#     print('Digite um horário entre 0-24')
+#         if hora <= 11:
+#             print('Bom dia!')
+#         elif hora <= 17:
+#             print('Boa tarde!')
+#         else:
+#             print('boa noite!')
+
+try:
+    hora = int(hora)
+    if 0 <= hora <= 11:
+        print('Bom dia')
+    elif 12 <= hora <= 17:
+        print('Boa tarde')
+    elif 18 <= hora <= 23:
+        print('Boa noite')
+    else:
+        print('horário invalido')
+except:
+    print('Digite um horário entre 0-24')
 
 """
 # Faça  um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou
@@ -89,19 +89,19 @@ qtd_letras = input('Digite seu primeiro nome: ')
 #     print("Seu nome é normal")
 
 
-if len(qtd_letras) <= 4:
-    print("Seu nome é curto")
-elif len(qtd_letras) >= 6:
-    print("Seu nome é muito grande")
-else:
-    print("Seu nome é normal")
-
-
-# tamanho = len(qtd_letras)
-#
-# if tamanho <= 4:
-#     print('seu nome é curto')
-# elif tamanho <= 6:
-#     print('Seu nome é normal')
+# if len(qtd_letras) <= 4:
+#     print("Seu nome é curto")
+# elif len(qtd_letras) >= 6:
+#     print("Seu nome é muito grande")
 # else:
-#     print('Seu nome é muito grande')
+#     print("Seu nome é normal")
+
+
+tamanho = len(qtd_letras)
+
+if tamanho <= 4:
+    print('seu nome é curto')
+elif tamanho <= 6:
+    print('Seu nome é normal')
+else:
+    print('Seu nome é muito grande')
